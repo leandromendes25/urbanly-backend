f43bfec8fafec83467289b16cecdc855ffd4df82
leandromendes25 <leandromiendes@hotmail.com>
2026-05-04 15:06:09 -0300

    primeiro commit

---
+++ src/test/java/com/leandromendes25/urbanly/UrbanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandromendes25.urbanly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
